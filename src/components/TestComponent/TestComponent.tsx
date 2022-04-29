38e821a7f8587c7be242d20bc12014e21957385a
mdodell <dodellmitchell@gmail.com>
2022-04-29 15:06:39 -0700

    Added linting with ESLint and Prettier

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 interface TestComponentProps {
-  name: string;
+	name: string;
 }
 
 function TestComponent({ name }: TestComponentProps) {
-  return <h1>Hello {name}</h1>;
+	return <h1>Hello {name}</h1>;
 }
 
 export default TestComponent;
